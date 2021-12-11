22a083e21edd79ca732c9a4ad10d4cb8e9ae8112
dpc10ster <dpc10ster@gmail.com>
2021-12-10 22:47:34 -0500

    master to main

--- _deploy.sh
+++ _deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "main" ] && exit 0
 
 git config --global user.email "dpc10ster@gmail.com"
 git config --global user.name "dpc10ster"
